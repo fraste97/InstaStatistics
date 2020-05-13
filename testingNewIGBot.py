cd161f0a864356c00ccc1640b0be0fbdac0dc493
fraste97 <frati.stefano97@gmail.com>
2020-05-13 16:13:57 +0200

    Fixed code + comments

--- testingNewIGBot.py
+++ testingNewIGBot.py
@@ -40,28 +40,33 @@ class IGBot:
         self.logout()
         self.driver.quit()
 
+    # it returns the followers' list
     def get_follower_list(self):
         return self.get_username_list("/html/body/div[1]/section/main/div/header/section/ul/li[2]/a")
 
+    # it returns the followings' list
     def get_following_list(self):
         return self.get_username_list("/html/body/div[1]/section/main/div/header/section/ul/li[3]/a")
 
     # it opens the whole list of usernames
     def open_the_whole_list(self, xpath):
         self.driver.find_element_by_xpath(xpath).click()
-
         sleep(1)
-
+        # find the scroll box
         scroll_box = self.driver.find_element_by_xpath("/html/body/div[4]/div/div[2]")
-
+        # saves the initial scroll box's height
         last_height = self.driver.execute_script("return arguments[0].scrollHeight;", scroll_box)
+
         sleep(1)
         condition = True
+        # it scrolls the box to the end
         while condition:
+            # it scrolls the box to the (loaded) end
             self.driver.execute_script("arguments[0].scrollTo(0, arguments[0].scrollHeight);", scroll_box)
             sleep(0.6)
+            # it saves the scroll bar's height after scrolling
             new_height = self.driver.execute_script("return arguments[0].scrollHeight;", scroll_box)
-
+            # it compares the two heights in order to understand if the scroll box's end has been reached
             if new_height == last_height:
                 condition = False
 
@@ -69,21 +74,18 @@ class IGBot:
 
     # it returns a list of usernames
     def get_username_list(self, xpath):
-
+        # it opens the whole list by a scrolling operation
         self.open_the_whole_list(xpath)
 
         # it initializes a list of web elements that have a particular class name
-        names_links_list = self.driver.find_element_by_xpath("/html/body/div[4]/div/div[2]").find_elements_by_class_name("FPmhX")
-
-
-        print(names_links_list)
+        web_elements_list = self.driver.find_elements_by_class_name("FPmhX")
         username_list = []
         # it saves the textual content of every web element into another list
-        for user_name in names_links_list:
+        for user_name in web_elements_list:
             username_list.append(user_name.text + "\n")
-
+        # closes the scrollbox
         self.driver.find_element_by_xpath("/html/body/div[4]/div/div[1]/div/div[2]/button").click()
-        print(username_list)
+
         return username_list
 
 
